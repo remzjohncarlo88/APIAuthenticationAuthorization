d2f87df71aace1e2661ed5169618356ad15ab2e0
John <jcsay21@gmail.com>
2024-01-22 16:34:54 +0800

    Added Authentication files.

---
+++ APIAuthenticationAuthorization/Authentication/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace APIAuthenticationAuthorization.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ APIAuthenticationAuthorization/Authentication/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace APIAuthenticationAuthorization.Authentication
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
